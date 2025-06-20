055c042f33f8a811d962477315b773022ef0bb36
SonFire03 <sofiane.dehimi03@gmail.com>
2025-06-20 02:45:06 +0200

    Ajout de la page secrète avec musique

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
   this.reset();
 });
 
+// Affiche le bouton au scroll
+window.onscroll = function () {
+  const btn = document.getElementById("backToTop");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+// Scroll vers le haut quand on clique
+document.getElementById("backToTop").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
